633316d3d31aaa50c513c8c5da52706f15f4b5b7
Mark May <mamark90@gmail.com>
2019-05-21 01:48:52 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,12 +4,15 @@ def my_find(collection)
   i = 0
 
   while i < collection.length
-    yield(collection[i])
+    if yield(collection[i]) == true
+      return collection[i]
+      break
+    else
     i+=1
-
   end
 
 end
 
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+  
